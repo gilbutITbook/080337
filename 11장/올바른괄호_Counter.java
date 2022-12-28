63fc49818937a1556035a8a79bc41b354b4daa11
gilbutitbook <gilbutitbook@gmail.com>
2022-12-28 15:16:57 +0900

    initial release

--- 11장/올바른괄호_Counter.java
+++ 11장/올바른괄호_Counter.java
@@ -1,21 +1,6 @@
 import java.util.Stack;
 
 public class Solution {
-    private void findPair(char[] str) {
-        Stack<Integer> stack = new Stack<>();
-        for (int i = 0; i < str.length; i++) {
-            switch (str[i]) {
-                case '(' -> stack.push(i);
-                case ')' -> {
-                    if (stack.isEmpty()) return;
-
-                    // 짝이 되는 괄호의 인덱스 출력
-                    System.out.println(stack.pop() + ", " + i);
-                }
-            }
-        }
-    }
-
     public boolean solution(String s) {
         int counter = 0;
 
--- 7장/K번째수.java
+++ 7장/K번째수.java
@@ -7,8 +7,8 @@ public class Solution {
         for (int i = 0; i < answer.length; i++) {
             int[] command = commands[i];
             int from = command[0] - 1;  // 0-base
-            int to = command[1];  // exclusive
-            int k = command[2] - 1;  // 0-base
+            int to = command[1];        // exclusive
+            int k = command[2] - 1;     // 0-base
 
             int[] sub = Arrays.copyOfRange(array, from, to);
             Arrays.sort(sub);
