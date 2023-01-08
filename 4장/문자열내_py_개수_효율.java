670347cc3e49ba4eae64838917f1a810b04ebb42
CubeDr <spaceship00@naver.com>
2023-01-08 11:47:53 +0900

    문자열 내 py 개수 문제 인덴트 수정
    tabs 대신 spaces 사용

--- 4장/문자열내_py_개수_효율.java
+++ 4장/문자열내_py_개수_효율.java
@@ -1,15 +1,15 @@
 public class Solution {
     boolean solution(String s) {
-		int ps = 0;
-		int ys = 0;
+        int ps = 0;
+	    int ys = 0;
 
-		for (char c : s.toCharArray()) {
-			switch (c) {
-				case 'p', 'P' -> ps++;
-				case 'y', 'Y' -> ys++;
-			}
-		}
+        for (char c : s.toCharArray()) {
+            switch (c) {
+                case 'p', 'P' -> ps++;
+                case 'y', 'Y' -> ys++;
+            }
+        }
 
-		return ps == ys;
-	}
+        return ps == ys;
+    }
 }
